17498c12fed034c3a8a020a651e6d2ef3a36542f
Aske Djupnes Ammentorp <askeda@gmail.com>
2021-12-17 13:32:13 +0100

    Api, and fetching data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,28 @@
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import { API_URL } from '@/config/index';
 
-export default function EventsPage() {
-    return (
-        <Layout>
-            <h1>Events</h1>
-        </Layout>
-    )
+export default function EventsPage({events}) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      <ul>
+        {events.map(evt => (
+          <EventItem key={evt.id} evt={evt} />
+        ))}
+      </ul>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
 }
