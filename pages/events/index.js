16294c8fb4c08e4163ef827db39ecf80e08e03a8
Aske Djupnes Ammentorp <askeda@gmail.com>
2022-01-11 15:41:10 +0100

    Pagination

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,9 @@
 import Layout from '@/components/Layout';
 import EventItem from '@/components/EventItem';
 import { API_URL } from '@/config/index';
+import qs from 'qs';
+
+const PER_PAGE = 2;
 
 export default function EventsPage({events}) {
   return (
@@ -17,12 +20,20 @@ export default function EventsPage({events}) {
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events?_sort=date:ASC&populate=image`);
+export async function getServerSideProps({query: {page = 1}}) {
+  const params = {
+    pagination: {
+      page: page,
+      pageSize: PER_PAGE
+    },
+    populate: 'image'
+}
+
+const query = qs.stringify(params);
+  const res = await fetch(`${API_URL}/api/events?_sort=date:ASC&${query}`);
   const events = await res.json();
 
   return {
     props: { events: events.data },
-    revalidate: 1,
   }
 }
