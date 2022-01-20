b4bd77b80b087725d3769f24ae5d9a2e8adc9011
Aske Djupnes Ammentorp <askeda@gmail.com>
2022-01-20 11:14:40 +0100

    Added origin

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
             source: "/api/:path*",
             headers: [
               { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
+              { key: "Access-Control-Allow-Origin", value: "https://djevents-xi.vercel.app" },
               {
                 key: "Access-Control-Allow-Methods",
                 value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
@@ -20,7 +20,6 @@ module.exports = {
                   "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
               },
             ],
-            origin: ['https://djevents-xi.vercel.app', 'https://djevents.vercel.app']
           },
         ];
       },
