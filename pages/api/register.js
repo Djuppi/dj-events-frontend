5aee1b63cb1ce2ffe0cc971f2340624b1fc41d6c
Aske Djupnes Ammentorp <askeda@gmail.com>
2022-01-20 13:41:14 +0100

    Editted register

--- pages/api/register.js
+++ pages/api/register.js
@@ -17,7 +17,7 @@ export default async (req, res) => {
             })
         })
 
-        const data = await strapiRes.json() // Returns 500 error
+        const data = await strapiRes.json();
 
         if(strapiRes.ok) {
             // Set Cookie
